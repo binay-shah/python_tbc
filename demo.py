157c0af62bdfa613612f0bc683f6cd4ffd337cbd
binay-shah <binay18.shah@gmail.com>
2019-08-28 07:11:09 +0545

    web_services

--- demo.py
+++ demo.py
@@ -1,12 +1,5 @@
-import sqlite3
+import urllib.request
 
-conn = sqlite3.connect("phones.db")
-cursor = conn.cursor()
-#cursor.execute("drop table  if exists phones")
-#cursor.execute("create table phones (name varchar(100), ph_num INTEGER)")
-cursor.execute("insert into phones values('test', 788987)")
-cursor.execute("commit")
-cursor.execute("select * from phones")
-results = cursor.fetchall()
-print(results)
-conn.close()
+fhand = urllib.request.urlopen('https://docs.python.org/3/library/')
+for line in fhand:
+    print(line.decode().strip())
