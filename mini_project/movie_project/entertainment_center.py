79ac2c398255648c0278e183e567e0e3cbfb1d42
binay-shah <binay18.shah@gmail.com>
2019-08-30 08:56:14 +0545

    update

--- mini_project/movie_project/entertainment_center.py
+++ mini_project/movie_project/entertainment_center.py
@@ -2,11 +2,12 @@ import media
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
+import fresh_tomatoes
 
 
 YOUTUBE_WEB_URL = "https://www.youtube.com/watch?v="
 api_key = '34b041a422f882244116c2c49cfe9554'
-const val IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185"
+IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185"
 # https://developers.google.com/maps/documentation/geocoding/intro
 
 popular_movies_url = 'https://api.themoviedb.org/3/movie/popular?api_key=34b041a422f882244116c2c49cfe9554'
@@ -25,6 +26,7 @@ except:
     js = None
 
 results = js['results']
+print(results)
 
 
 movies = []
@@ -35,11 +37,14 @@ for movie in results:
     video_response = urllib.request.urlopen(movie_videos_url, context=ctx)
     data = video_response.read().decode()
     js = json.loads(data)
-    print(js)
-    trailer_url = YOUTUBE_WEB_URL + js['results'][0]['key']
     
-    break
-
+    trailer_url = YOUTUBE_WEB_URL + js['results'][0]['key']
+    image_path = IMAGE_BASE_URL + movie['poster_path']
+    obj =  media.Movie(movie['title'], movie['overview'], image_path, trailer_url)
+    movies.append(obj)
+    
+    
+fresh_tomatoes.open_movies_page(movies)
 
 
 #movie = media.Movie()
